8298412ae68e411e32780287b31cf6a5dfc5006f
Levon <chahestian@hotmail.com>
2024-04-17 19:21:06 -0300

    add currency pipe

--- src/app/tree/tree.component.ts
+++ src/app/tree/tree.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { IBudget } from '../interfaces/budget';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-tree',
   standalone: true,
+  imports: [CurrencyPipe],
   templateUrl: './tree.component.html',
   styleUrl: './tree.component.scss'
 })
